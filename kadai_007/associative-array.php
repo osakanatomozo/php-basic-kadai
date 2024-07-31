579b83f7161d9bc8a5724a1ffe5a0cce9eb82d8f
osakanatomozo <0yj3251k2p70v9f@i.softbank.jp>
2024-07-31 14:10:09 +0900

    kadai_007をアップロード
    kadai_007をアップロードしました。

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>7章課題</title>
+<head>
+
+<body>
+  <p>
+    <?php
+    // 連想配列に値を代入する
+    $product_data=['name'=>'onion', 'price'=>200, 'weight'=>160];
+
+    // 連想配列の値を出力する
+    print_r($product_data);
+    ?>
+  </p>
+</body>
+
+</html>
