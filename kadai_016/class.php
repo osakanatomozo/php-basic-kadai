285873bd40bb13d2f76fa2a3f8ffe35822cdc69c
osakanatomozo <0yj3251k2p70v9f@i.softbank.jp>
2024-08-07 16:12:55 +0900

    kadai_016をアップロード
    kadai_016をアップロードしました。

---
+++ kadai_016/class.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <title>16章課題</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    class Food {
+      private $name;
+      private $price;
+
+      public function __construct(string $name, int $price) {
+        $this->name=$name;
+        $this->price=$price;
+      }
+      public function show_price() {
+        echo $this->price. '<br>';
+      }
+    }
+
+    class Animal {
+      private $name;
+      private $height;
+      private $weight;
+
+      public function __construct(string $name, int $height, int $weight) {
+        $this->name=$name;
+        $this->height=$height;
+        $this->weight=$weight;
+      }
+      public function show_height() {
+        echo $this->height. '<br>';
+      }
+    }
+
+    $animal=new Animal('dog',60,5000);
+    $food=new Food('potato',250);
+
+    print_r($food);
+    echo '<br>';
+    print_r($animal);
+    echo '<br>';
+
+    $food->show_price();
+    $animal->show_height();
+    ?>
+  </p>
+</body>
+
+<html>
