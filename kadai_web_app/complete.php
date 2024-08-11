627a5f8c50e857d77ead1bc98ac653e65ae5cd41
osakanatomozo <0yj3251k2p70v9f@i.softbank.jp>
2024-08-11 18:49:00 +0900

    kadai_web_appをアップロード
    kadai_web_appをアップロードしました。

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charsset="utf-8">
+  <title>PHP基礎編課題</title>
+</head>
+
+<body>
+  <h2>登録が完了しました。<h2>
+  <p>社員情報がデータベースに保存されました。</p>
+
+  <button id="back-btn" onclick="location.href='form.php';">戻る</button>
+</body>
+
+</html>
